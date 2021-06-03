cde49e143245346b9d5f78db05f3c7a4f17913fb
Esteban Burgos <estebanburgos85@gmail.com>
2021-06-03 15:31:52 -0300

    Add button navigation for new customer form and add react-native-vector-icons config

--- App.js
+++ App.js
@@ -3,11 +3,27 @@ import React from 'react';
 import {StyleSheet,Text,View} from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
+
+// Components
+import Header from './components/ui/Header';
+
+// Views
 import Home from './views/Home';
 import NewCustomer from './views/NewCustomer';
 import Details from './views/Details';
 
+// Creamos Stack para las diferentes pantallas
 const Stack = createStackNavigator();
+
+// Creamos el theme con react-native-paper
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1774F2',
+  }
+}
 
 
 const App = () => {
@@ -17,10 +33,29 @@ const App = () => {
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="Inicio"
+          screenOptions={{
+              headerTitleAlign: 'center',
+              headerStyle: {
+                backgroundColor: theme.colors.primary,
+              },
+              headerTintColor: theme.colors.surface,
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                textTransform: 'uppercase'
+              },
+            }}
         >
           <Stack.Screen
             name="Inicio"
             component={Home}
+            options={({navigation, route}) => ({
+              headerLeft: (props) => (
+                <Header {...props}
+                  navigation={navigation} 
+                  route={route}
+                />
+              )
+            })}
           />
           <Stack.Screen
             name="NuevoCliente"
